fb69ba69cce4aef110eb906807bec9eb9ba12fd3
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2024-05-03 17:34:39 -0300

    Refactor Point struct and functions in point.h

--- src/TAD/point/point.h
+++ src/TAD/point/point.h
@@ -5,18 +5,59 @@
 
 typedef struct point Point;
 
+/**
+ * @brief Create a Point object
+ *
+ * @param x
+ * @param y
+ * @return Point*
+ */
 Point *createPoint(float x, float y);
 
+/**
+ * @brief Free a Point object
+ *
+ * @param point
+ */
 void freePoint(Point **point);
 
+/**
+ * @brief Print a Point object
+ *
+ * @param point
+ */
 void printPoint(Point *point);
 
+/**
+ * @brief Get the X object
+ *
+ * @param point
+ * @return float
+ */
 float getX(Point *point);
 
+/**
+ * @brief Get the Y object
+ *
+ * @param point
+ * @return float
+ */
 float getY(Point *point);
 
+/**
+ * @brief Set the X object
+ *
+ * @param point
+ * @param x
+ */
 void setX(Point *point, float x);
 
+/**
+ * @brief Set the Y object
+ *
+ * @param point
+ * @param y
+ */
 void setY(Point *point, float y);
 
 #endif /* POINT_H */
